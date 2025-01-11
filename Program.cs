61b2db4c989682218c4a74d19c6c66b8c07439fa
leobaiao <leobaiao966@gmail.com>
2025-01-11 18:17:39 -0300

    calculo simples adicionado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("em produção");
+﻿using System;
+
+namespace Calculadora
+{
+        class programa
+        {
+             static void Main (string[] strings)
+             {
+            Console.WriteLine("Digite o primeiro numero: ");
+              int num1 = int.Parse(Console.ReadLine());
+           
+            Console.WriteLine("Digite a operação: ");
+                string operacao = Console.ReadLine();
+
+            Console.WriteLine("Digite o segundo numero: ");
+              int num2 = int.Parse(Console.ReadLine());
+                
+                switch (operacao)
+                {
+                    case "+":
+                        Console.WriteLine("Resultado: " + (num1 + num2));
+                        break;
+                    case "-":
+                        Console.WriteLine("Resultado: " + (num1 - num2));
+                        break;
+                    case "*":
+                        Console.WriteLine("Resultado: " + (num1 * num2));
+                        break;
+                    case "/":
+                    if (num1 == 0 || num2 == 0){
+                        Console.WriteLine("Não é possível dividir por zero");
+                    }else{
+                        double num01 = Convert.ToDouble(num1);
+                        Console.WriteLine("Resultado: " + (num01 / num2));
+                    }
+                        break;
+                    default:
+                        Console.WriteLine("Operação inválida");
+                        break;
+                }  
+             }
+        }
+}
